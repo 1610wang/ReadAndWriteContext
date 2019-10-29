cb4cf01405f1a9401f19ef0691a1ec61f0562abb
ZhangHu <1095003412@qq.com>
2019-10-29 15:59:46 +0800

    excel模板

--- src/main/java/com/wxy/pojo/Excel.java
+++ src/main/java/com/wxy/pojo/Excel.java
@@ -17,8 +17,9 @@ import java.io.IOException;
  */
 public class Excel {
     /**
-     * 将解析获取到的内容写入到excel中
-     * @param s
+     *  创建一个excel文件并将分析内容写入其中
+     * @param s 分析所得内容
+     * @param path excel文件目标地址
      */
     public static void WriteContent(String[] s,String path){
         //第一步，创建一个workbook对应一个excel文件
@@ -26,7 +27,7 @@ public class Excel {
         //入云用户和系统情况
         HSSFSheet sheet1 = creatFirSheet(workbook);
         //资源使用情况
-        HSSFSheet sheet2 = workbook.createSheet("资源使用情况");
+        HSSFSheet sheet2 =creatSecSheet(workbook);
         //云内系统排名时间时间
         String[] cellNames = {"时间（周）","系统访问总量前10名"};
         HSSFSheet sheet3 = creatSheet(workbook,"云内系统排名时间时间",cellNames);
@@ -97,6 +98,12 @@ public class Excel {
             return null;
         }
     }
+
+    /**
+     * 入云用户和系统使用情况sheet模板
+     * @param workbook
+     * @return
+     */
     public static HSSFSheet creatFirSheet(HSSFWorkbook workbook){
         HSSFCellStyle style = workbook.createCellStyle();
         style.setAlignment(HorizontalAlignment.CENTER);
@@ -161,5 +168,90 @@ public class Excel {
             cellTemp.setCellStyle(style);
         }
         return sheet;
+    }
+
+    /**
+     * 资源使用情况sheet模板
+     * @param workbook
+     * @return
+     */
+    public static HSSFSheet creatSecSheet(HSSFWorkbook workbook){
+        HSSFCellStyle style = workbook.createCellStyle();
+        style.setAlignment(HorizontalAlignment.CENTER);
+        HSSFSheet sheet = workbook.createSheet("资源使用情况");
+        sheet.addMergedRegion(new CellRangeAddress(0,2,0,0));
+        sheet.addMergedRegion(new CellRangeAddress(0,0,1,28));
+        sheet.addMergedRegion(new CellRangeAddress(0,0,29,56));
+        sheet.addMergedRegion(new CellRangeAddress(0,0,57,84));
+        sheet.addMergedRegion(new CellRangeAddress(0,1,85,91));
+        sheet.addMergedRegion(new CellRangeAddress(0,1,92,98));
+        sheet.addMergedRegion(new CellRangeAddress(0,1,99,105));
+        sheet.addMergedRegion(new CellRangeAddress(0,1,106,112));
+        sheet.addMergedRegion(new CellRangeAddress(0,1,113,120));
+
+        sheet.addMergedRegion(new CellRangeAddress(1,1,1,7));
+        sheet.addMergedRegion(new CellRangeAddress(1,1,8,14));
+        sheet.addMergedRegion(new CellRangeAddress(1,1,15,21));
+        sheet.addMergedRegion(new CellRangeAddress(1,1,22,28));
+        sheet.addMergedRegion(new CellRangeAddress(1,1,29,35));
+        sheet.addMergedRegion(new CellRangeAddress(1,1,36,42));
+        sheet.addMergedRegion(new CellRangeAddress(1,1,43,49));
+        sheet.addMergedRegion(new CellRangeAddress(1,1,50,56));
+        sheet.addMergedRegion(new CellRangeAddress(1,1,57,63));
+        sheet.addMergedRegion(new CellRangeAddress(1,1,64,70));
+        sheet.addMergedRegion(new CellRangeAddress(1,1,71,77));
+        sheet.addMergedRegion(new CellRangeAddress(1,1,78,84));
+
+//        //设置第一行表头
+        HSSFRow row1 = sheet.createRow(0);
+        HSSFCell cell1 = row1.createCell(0);
+        cell1.setCellValue("时间（周）");
+        cell1.setCellStyle(style);
+        HSSFCell cell2 = row1.createCell(1);
+        cell2.setCellValue("vCPU(核）");
+        cell2.setCellStyle(style);
+        HSSFCell cell3 = row1.createCell(29);
+        cell3.setCellValue("内存（GB)");
+        cell3.setCellStyle(style);
+        HSSFCell cell4 = row1.createCell(57);
+        cell4.setCellValue("存储（TB)");
+        cell4.setCellStyle(style);
+        HSSFCell cell5 = row1.createCell(85);
+        cell5.setCellValue("设备总数");
+        cell5.setCellStyle(style);
+        HSSFCell cell6 = row1.createCell(92);
+        cell6.setCellValue("计算资源池服务器（台）");
+        cell6.setCellStyle(style);
+        HSSFCell cell7 = row1.createCell(99);
+        cell7.setCellValue("租用物理服务器（台）");
+        cell7.setCellStyle(style);
+        HSSFCell cell8 = row1.createCell(106);
+        cell8.setCellValue("比例");
+        cell8.setCellStyle(style);
+        HSSFCell cell9 = row1.createCell(113);
+        cell9.setCellValue("其他物理设备数量（台）");
+        cell9.setCellStyle(style);
+        //设置第二行和第三行表头
+        HSSFRow row2 = sheet.createRow(1);
+        HSSFRow row3 = sheet.createRow(2);
+        String[] title = {"已使用","总量","占比","同比增长"};
+        String[] conten = {"合计","太极云","金山云","首信云","联通云","浪潮云","电信云"};
+        int i = 1;
+        int n = 1;
+        for (int j = 0; j < 3; j++) {
+            for (String s : title) {
+                HSSFCell hssfCell = row2.createCell(i);
+                hssfCell.setCellValue(s);
+                hssfCell.setCellStyle(style);
+                i += 7;
+                for (String s1 : conten) {
+                    HSSFCell hssfCell3 = row3.createCell(n);
+                    hssfCell3.setCellValue(s1);
+                    hssfCell3.setCellStyle(style);
+                    n++;
+                }
+            }
+        }
+        return null;
     }
 }
